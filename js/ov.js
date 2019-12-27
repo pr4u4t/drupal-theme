2aafb6929eea52dc5afd1d8607d23827b623aac4
Pawel Ciejka <pawel.ciejka@robco.pl>
2019-12-27 12:09:56 +0000

    initial commit of eprawnik theme

---
+++ js/ov.js
@@ -0,0 +1,32 @@
+(function ($, Drupal) {
+  Drupal.behaviors.robcosetupOverlay = {
+    attach: function (context, settings) {
+	$('.main-navigation-block .menu .dropdown', context).once('setupOverlay').each(function () {
+        $(this).hover(function(event){
+		console.log("HOVER");
+		var fc = $(this).parent();
+		if($(this).length === 0) return;
+
+		var sc = fc.parent();
+		if(sc.length === 0) return;
+
+		if(!($('.main-navigation-block').is(fc) || $('.main-navigation-block').is(sc))){
+			if(sc.hasClass('.dropdown-menu') || fc.hasClass('.dropdown-menu'))  
+				$('#dim-overlay').show();
+			return;
+		}
+
+		if( $(this).find(".dropdown-menu").length !== 0 )
+			$('#dim-overlay').show();
+		else
+			$('#dim-overlay').hide();
+
+	},function(event){
+		if(!$('.main-navigation-block').is(':hover'))
+			$('#dim-overlay').hide();
+	});
+      });
+    }
+  };
+})(jQuery, Drupal);
+
