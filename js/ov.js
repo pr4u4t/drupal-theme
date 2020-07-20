abb1be7c7ed9f0efa0123d1e3d3116a9864646be
Bartosz Sadowy <bartosz.sadowy@robco.pl>
2020-07-20 11:38:31 +0000

    Navbar fixes with js

--- js/ov.js
+++ js/ov.js
@@ -1,32 +1,40 @@
 (function ($, Drupal) {
   Drupal.behaviors.robcosetupOverlay = {
     attach: function (context, settings) {
-	$('.main-navigation-block .menu .dropdown', context).once('setupOverlay').each(function () {
-        $(this).hover(function(event){
-		
-		var fc = $(this).parent();
-		if($(this).length === 0) return;
-
-		var sc = fc.parent();
-		if(sc.length === 0) return;
-
-		if(!($('.main-navigation-block').is(fc) || $('.main-navigation-block').is(sc))){
-			if(sc.hasClass('.dropdown-menu') || fc.hasClass('.dropdown-menu'))  
-				$('#dim-overlay').show();
-			return;
-		}
-
-		if( $(this).find(".dropdown-menu").length !== 0 )
-			$('#dim-overlay').show();
-		else
-			$('#dim-overlay').hide();
-
-	},function(event){
-		if(!$('.main-navigation-block').is(':hover'))
-			$('#dim-overlay').hide();
-	});
-      });
-    }
+			$('.main-navigation-block .menu .dropdown', context).once('setupOverlay').each(function () {
+  		  $(this).hover(function(event){
+				
+					var fc = $(this).parent();
+					if($(this).length === 0) return;
+				
+					var sc = fc.parent();
+					if(sc.length === 0) return;
+				
+					if(!($('.main-navigation-block').is(fc) || $('.main-navigation-block').is(sc))){
+						if(sc.hasClass('.dropdown-menu') || fc.hasClass('.dropdown-menu'))  
+						$('#dim-overlay').show();
+						return;
+					}
+				
+					if( $(this).find(".dropdown-menu").length !== 0 )
+						$('#dim-overlay').show();
+					else
+						$('#dim-overlay').hide();
+				
+				},function(event){
+					if(!$('.main-navigation-block').is(':hover'))
+						$('#dim-overlay').hide();
+				});
+			});
+			
+			$('#edit-keys').onfocus(
+				function(){
+					$('#navbar-search').addClass('search-focus');
+				},
+				function(){
+					$('#navbar-search').removeClass('search-focus');
+				}
+			);
+  	}
   };
 })(jQuery, Drupal);
-
